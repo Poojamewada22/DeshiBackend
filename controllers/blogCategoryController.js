af45b222f8ce22273942b57f01acb1165434a59c
Pooja Meweda <poojamewada@bakhultech.com>
2026-05-30 00:41:45 +0530

    uploade

--- controllers/blogCategoryController.js
+++ controllers/blogCategoryController.js
@@ -1,106 +1,78 @@
-// const Category = require("../models/blogCategory");
-
-// const BlogCategory = require("../models/blogCategory");
 const BlogCategory = require("../models/blogCategory");
-// ================= CREATE =================
-const createCategory = async (
-  req,
-  res
-) => {
 
+// Create Category
+exports.createCategory = async (req, res) => {
   try {
-
     const { name } = req.body;
 
-    if (!name) {
-
-      return res.status(400).json({
-        success: false,
-        message: "Category name required",
-      });
-    }
-
-    const category =
-      await Category.create({
-        name,
-      });
+    const category = new BlogCategory({ name });
+    await category.save();
 
     res.status(201).json({
       success: true,
-      message: "Category Created",
+      message: "Category created successfully",
       category,
     });
-
   } catch (error) {
-
-    console.log(error);
-
     res.status(500).json({
       success: false,
-      message: "Server Error",
+      message: error.message,
     });
   }
 };
 
-// ================= GET =================
-const getCategories = async (
-  req,
-  res
-) => {
-
+// Get All Categories
+exports.getCategories = async (req, res) => {
   try {
-
-    const categories =
-      await Category.find().sort({
-        createdAt: -1,
-      });
+    const categories = await BlogCategory.find();
 
     res.status(200).json({
       success: true,
       categories,
     });
-
   } catch (error) {
-
-    console.log(error);
-
     res.status(500).json({
       success: false,
-      message: "Server Error",
+      message: error.message,
     });
   }
 };
 
-// ================= DELETE =================
-const deleteCategory = async (
-  req,
-  res
-) => {
-
+// Update Category
+exports.updateCategory = async (req, res) => {
   try {
-
-    await Category.findByIdAndDelete(
-      req.params.id
+    const category = await BlogCategory.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
     );
 
     res.status(200).json({
       success: true,
-      message: "Deleted",
+      message: "Category updated successfully",
+      category,
     });
-
   } catch (error) {
-
-    console.log(error);
-
     res.status(500).json({
       success: false,
-      message: "Server Error",
+      message: error.message,
     });
   }
 };
 
-module.exports = {
-  createCategory,
-  getCategories,
-  deleteCategory,
+// Delete Category
+exports.deleteCategory = async (req, res) => {
+  try {
+    await BlogCategory.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      success: true,
+      message: "Category deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
